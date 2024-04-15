53c6361cff5fb7693af9a222bc95135ae744fd23
Ottatop <Ottatop@users.noreply.github.com>
2024-04-15 20:51:13 +0000

    deploy: pinnacle-comp/pinnacle@ac2ea95c1fc979435a0981dc22e4edc2b7a48ef0

--- api_enhancements/pinnacle_api/output/sidebar-items.js
+++ api_enhancements/pinnacle_api/output/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Alignment"],"struct":["Mode","Output","OutputHandle","OutputProperties"]};
+window.SIDEBAR_ITEMS = {"enum":["Alignment","OutputMatcher"],"struct":["Mode","Output","OutputHandle","OutputProperties","OutputSetup"]};
