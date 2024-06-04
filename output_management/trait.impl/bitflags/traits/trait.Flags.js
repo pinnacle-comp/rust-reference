dd97cb7ea09b7c2b063a91abb181513d8c8c2857
Ottatop <Ottatop@users.noreply.github.com>
2024-06-04 15:49:44 +0000

    deploy: pinnacle-comp/pinnacle@6568b30c6af606009a8d3c5186bfd78812c71c46

---
+++ output_management/trait.impl/bitflags/traits/trait.Flags.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pinnacle_api":[["impl Flags for <a class=\"struct\" href=\"pinnacle_api/output/struct.UpdateLocsOn.html\" title=\"struct pinnacle_api::output::UpdateLocsOn\">UpdateLocsOn</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
