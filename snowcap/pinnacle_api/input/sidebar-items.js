05d1d2bf3d7b773610298700fdd6514da5c55dab
Ottatop <Ottatop@users.noreply.github.com>
2024-06-15 19:07:27 +0000

    deploy: pinnacle-comp/pinnacle@6d5285d8aa5bdd606b7e80327b4886a19b08fd21

--- snowcap/pinnacle_api/input/sidebar-items.js
+++ snowcap/pinnacle_api/input/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Mod","MouseButton","MouseEdge"],"mod":["libinput"],"struct":["Input","XkbConfig"],"trait":["Key"]};
+window.SIDEBAR_ITEMS = {"enum":["Mod","MouseButton","MouseEdge"],"mod":["libinput"],"struct":["Input","KeybindDescription","KeybindInfo","XkbConfig"],"trait":["Key"]};
