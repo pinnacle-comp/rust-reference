e2c62b87a3a84a527d7c2c126fb9765d5fb9c1d3
Ottatop <Ottatop@users.noreply.github.com>
2024-06-17 23:33:52 +0000

    deploy: pinnacle-comp/pinnacle@3a7d9e20c056eb13683121d7f294e10735f2e17a

--- snowcap-lua/trait.impl/core/str/traits/trait.FromStr.js
+++ snowcap-lua/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pinnacle_api":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"pinnacle_api/output/struct.Modeline.html\" title=\"struct pinnacle_api::output::Modeline\">Modeline</a>"]]
+"pinnacle_api":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"pinnacle_api/output/struct.Modeline.html\" title=\"struct pinnacle_api::output::Modeline\">Modeline</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
