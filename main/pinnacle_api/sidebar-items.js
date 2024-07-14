b3d0ab55458502ef8ce38372c14f3d7c17a3a74d
Ottatop <Ottatop@users.noreply.github.com>
2024-07-14 23:59:52 +0000

    deploy: pinnacle-comp/pinnacle@53ea4ebace598a3f361586782352ad5113ff1f7e

--- main/pinnacle_api/sidebar-items.js
+++ main/pinnacle_api/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["config"],"fn":["connect","listen"],"macro":["batch_boxed","batch_boxed_async"],"mod":["input","layout","output","pinnacle","process","render","signal","snowcap","tag","util","window"],"struct":["ApiModules","Receivers"]};
+window.SIDEBAR_ITEMS = {"attr":["config"],"fn":["connect","listen","set_default_tracing_subscriber"],"macro":["batch_boxed","batch_boxed_async"],"mod":["input","layout","output","pinnacle","process","render","signal","snowcap","tag","util","window"],"struct":["ApiModules"]};
