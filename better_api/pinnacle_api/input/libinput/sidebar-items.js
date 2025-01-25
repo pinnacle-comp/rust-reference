e88321e5dd23baf8cc171e1fcfd0dbd58ed54da3
Ottatop <Ottatop@users.noreply.github.com>
2025-01-25 03:56:46 +0000

    deploy: pinnacle-comp/pinnacle@a81ad4bc2716f2a4265773b7015ba09376eab6d6

--- better_api/pinnacle_api/input/libinput/sidebar-items.js
+++ better_api/pinnacle_api/input/libinput/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AccelProfile","ClickMethod","DeviceType","ScrollMethod","SendEventsMode","TapButtonMap"],"fn":["for_all_devices","get_devices"],"struct":["Capability","DeviceHandle"]};
+window.SIDEBAR_ITEMS = {"enum":["AccelProfile","ClickMethod","DeviceType","ScrollMethod","SendEventsMode","TapButtonMap"],"fn":["for_each_device","get_devices"],"struct":["Capability","DeviceHandle"]};
