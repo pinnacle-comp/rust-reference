fa4dc0e1981525dece7aea36c940e3915ca05bbb
Ottatop <Ottatop@users.noreply.github.com>
2025-02-09 05:29:31 +0000

    deploy: pinnacle-comp/pinnacle@ca3150ab2572690177df1203f8e5859271254a96

--- main/trait.impl/core/str/traits/trait.FromStr.js
+++ main/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["pinnacle_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"pinnacle_api/output/struct.Modeline.html\" title=\"struct pinnacle_api::output::Modeline\">Modeline</a>"]]]]);
+    var implementors = Object.fromEntries([["pinnacle_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"pinnacle_api/output/struct.Modeline.html\" title=\"struct pinnacle_api::output::Modeline\">Modeline</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
