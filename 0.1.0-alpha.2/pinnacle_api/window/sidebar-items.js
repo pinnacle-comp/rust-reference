0bfa2704db704c6ff8ee72c9d57b86aa098bd600
Ottatop <Ottatop@users.noreply.github.com>
2025-04-10 01:17:42 +0000

    deploy: pinnacle-comp/pinnacle@e3c5f082a86102e8dfbe56bbbdf8b2dcae8c096e

---
+++ 0.1.0-alpha.2/pinnacle_api/window/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DecorationMode","LayoutMode"],"fn":["add_window_rule","begin_move","begin_resize","connect_signal","get_all","get_all_async","get_focused","get_focused_async"],"struct":["WindowHandle"]};
