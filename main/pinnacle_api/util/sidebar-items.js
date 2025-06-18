252844f5be7ff8f86ce30d482c971e6882f9fe74
Ottatop <Ottatop@users.noreply.github.com>
2025-06-18 23:40:43 +0000

    deploy: pinnacle-comp/pinnacle@b15f1bba0d73b3497949d6301c937293d913f5ac

--- main/pinnacle_api/util/sidebar-items.js
+++ main/pinnacle_api/util/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Axis"],"fn":["batch","batch_async"],"macro":["batch_boxed","batch_boxed_async"],"struct":["Point","Rect","Size"],"trait":["Batch"]};
+window.SIDEBAR_ITEMS = {"enum":["Axis","Direction"],"fn":["batch","batch_async"],"macro":["batch_boxed","batch_boxed_async"],"struct":["Point","Rect","Size"],"trait":["Batch"]};
