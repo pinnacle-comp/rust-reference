9c58a955bbe5fc462ecbc5d21c816960e70cf276
Ottatop <Ottatop@users.noreply.github.com>
2025-07-02 22:22:42 +0000

    deploy: pinnacle-comp/pinnacle@e8b3b6383ab6cd287097007f004c55d74e8728ff

--- main/trait.impl/core/fmt/trait.Binary.js
+++ main/trait.impl/core/fmt/trait.Binary.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["pinnacle_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"pinnacle_api/input/libinput/struct.Capability.html\" title=\"struct pinnacle_api::input::libinput::Capability\">Capability</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"pinnacle_api/input/struct.Mod.html\" title=\"struct pinnacle_api::input::Mod\">Mod</a>"]]]]);
+    var implementors = Object.fromEntries([["pinnacle_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"pinnacle_api/input/libinput/struct.Capability.html\" title=\"struct pinnacle_api::input::libinput::Capability\">Capability</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"pinnacle_api/input/struct.Mod.html\" title=\"struct pinnacle_api::input::Mod\">Mod</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- main/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ main/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tokio",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"tokio/sync/mpsc/struct.PermitIterator.html\" title=\"struct tokio::sync::mpsc::PermitIterator\">PermitIterator</a>&lt;'a, T&gt;"]]]]);
+    var implementors = Object.fromEntries([["tokio",[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"tokio/sync/mpsc/struct.PermitIterator.html\" title=\"struct tokio::sync::mpsc::PermitIterator\">PermitIterator</a>&lt;'a, T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
