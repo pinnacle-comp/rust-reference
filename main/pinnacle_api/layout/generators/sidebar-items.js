5c8d25b5923b3c4f82c93c3c649741234ea373d8
Ottatop <Ottatop@users.noreply.github.com>
2025-07-08 17:37:01 +0000

    deploy: pinnacle-comp/pinnacle@a2271263cafcd0be3f2d0ef0bec7b18c72ddd707

--- main/pinnacle_api/layout/generators/sidebar-items.js
+++ main/pinnacle_api/layout/generators/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CornerLocation","MasterSide"],"struct":["Corner","Cycle","Dwindle","Fair","Line","MasterStack","Spiral"]};
+window.SIDEBAR_ITEMS = {"enum":["CornerLocation","MasterSide"],"struct":["Corner","Cycle","Dwindle","Fair","Floating","Line","MasterStack","Spiral"]};
